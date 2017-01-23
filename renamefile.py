2ca76c007538b8a4c0b8d73d07ad95a23e9e992f
laihouxin <elilai@qq.com>
2017-01-23 17:47:21 +0800

    init

---
+++ renamefile.py
@@ -0,0 +1,18 @@
+#! python3
+import os,time,shutil,sys
+print("Current work dir is", os.getcwd())
+if len(sys.argv) == 2:
+    workdir = sys.argv[1]
+else:
+    workdir = r'F:\bugs\screenshot'
+print('Change dir to', workdir)
+
+os.chdir(workdir)
+for filename in os.listdir("."):
+    suffix = os.path.splitext(filename)[-1] #取得后缀
+    if suffix == '.png' or suffix == '.jpg':
+        timeStamp = os.path.getctime(filename) #取得文件创建时间
+        timeArray = time.localtime(timeStamp)
+        formatTime = time.strftime("%Y-%m-%d-%H%M%S", timeArray) #间时间格式转换
+        shutil.move(filename,formatTime + suffix)
+        print("rename %s to %s" % (filename, formatTime + suffix))
---
+++ test.py
@@ -0,0 +1,25 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+['Alice', 'Bob', 'Carol', 'David'],
+['dogs', 'cats', 'moose', 'goose']]
+
+
+
+def printTable(tableData):
+    colWidths = [0] * len(tableData)
+    index = 0
+    for line in tableData:
+        maxlength = 0
+        for word in line:
+            if len(word) > maxlength:
+                maxlength = len(word)
+        colWidths[index] = maxlength
+        index+=1
+
+    print(colWidths)
+
+    for i in range(len(tableData[0])):
+        for j in range(len(tableData)):
+            print(tableData[j][i].ljust(colWidths[j]+1), end='')
+        print('')
+
+printTable(tableData)
